ff532d7e3c8e4d45881173479dca3cee12780154
minhdqpc06173 <minhdqpc06173@fpt.edu.vn>
2023-11-25 07:50:10 +0700

    xong lỗi

--- QuanLyShopDongHo/QuanLyShopDongHo/Properties/Resources.Designer.cs
+++ QuanLyShopDongHo/QuanLyShopDongHo/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace QuanLyShopDongHo.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap bg_dmk1 {
+            get {
+                object obj = ResourceManager.GetObject("bg_dmk1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap bg_kh {
             get {
                 object obj = ResourceManager.GetObject("bg_kh", resourceCulture);
